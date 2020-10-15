5b6494f9077580825f3e04af171513efdc4a1bf6
heyibrahimkhan@gmail.com <heyibrahimkhan@gmail.com>
2020-10-16 04:52:00 +0500

    added swicth and logic for verbosity.

--- siegma.py
+++ siegma.py
@@ -36,7 +36,7 @@ def setup_args():
 	parser.add_argument('-o', '--output', metavar='<output_file>', type=str, default='.output', help='Output file path. Eg: /path/to/output_file.')
 	parser.add_argument('-t', '--testing', dest='testing', action='store_true', help='Switch for testing. Default "False". If testing, output file will be created but the rule file will not be installed on SIEM. Eg: -t or --testing.')
 	parser.add_argument('-sbo', '--sigma_backend_options', dest='sigma_backend_options', action='store_true', help='Switch for enabling backend options feature of sigma. Default "False". Eg: -sbo or --sigma_backend_options.')
-	# parser.add_argument('-v', '--verbosity', metavar='<verbosity_level>', type=str, default='INFO', help='Execution verbosity level. Eg: SUCCESS|WARN|INFO|DEBUG.')
+	parser.add_argument('-v', '--verbosity', metavar='<verbosity_level>', type=str, default='DEBUG', help='Execution verbosity level. Eg: SUCCESS|WARN|INFO|DEBUG.')
 	logger.info('Arguments parsed successfully...')
 	return parser.parse_args()
 
@@ -69,6 +69,7 @@ def initialize_g_vars():
 	args.rule = args.rule.rstrip('/')
 	args.sigma_venv = args.sigma_venv.rstrip('\\')
 	args.sigma_venv = args.sigma_venv.rstrip('/')
+	if args.verbosity is not None and not args.verbosity: logger.setLevel(verbosity)
 	logger.info('initialize_g_vars() finished successfully...')
 
 
